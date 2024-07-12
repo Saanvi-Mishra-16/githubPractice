a32ec8d3ebe305859f1eb8f1e489f40c1d89fab8
Saanvi Mishra <saanmish26@gmail.com>
2024-07-12 14:56:15 -0400

    Initial Commit

---
+++ githubPractice/githubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubPracticeApp.swift
+//  githubPractice
+//
+//  Created by Meenakshi Mishra on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct githubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
